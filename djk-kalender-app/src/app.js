aa6ff656642511c7fb7714e6c9b1151d51a6122b
lukas <lukas.meinzer.98@web.de>
2025-01-26 10:31:25 +0000

    Liste verbessert. Sortierung nach Datum

--- djk-kalender-app/src/app.js
+++ djk-kalender-app/src/app.js
@@ -3,9 +3,16 @@ document.getElementById('requestButton').addEventListener('click', function() {
     fetch(`/get-ics-files/${selectedFolder}`)
         .then(response => response.json())
         .then(files => {
+            // Extract date from filename and sort files as strings
+            files.sort((a, b) => {
+                const dateA = a.split('_')[1].replace('.ics', '');
+                const dateB = b.split('_')[1].replace('.ics', '');
+                return dateA.localeCompare(dateB);
+            });
+
             const listContainer = document.getElementById('listContainer');
             listContainer.innerHTML = ''; // Clear any existing content
-            const ul = document.createElement('ul');
+            const ol = document.createElement('ol');
             files.forEach(file => {
                 const li = document.createElement('li');
                 const a = document.createElement('a');
@@ -13,9 +20,9 @@ document.getElementById('requestButton').addEventListener('click', function() {
                 a.textContent = file;
                 a.download = file;
                 li.appendChild(a);
-                ul.appendChild(li);
+                ol.appendChild(li);
             });
-            listContainer.appendChild(ul);
+            listContainer.appendChild(ol);
         })
         .catch(error => console.error('Error fetching files:', error));
 });
